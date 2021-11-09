4140bee35c3b7926e624c41b4c0627ff28c3dcb0
Bajkovski <34669317+Bajkovski@users.noreply.github.com>
2021-11-09 23:00:33 +0100

    SensorEventListener, customview call in MainActivity

--- app/src/main/java/com/example/gyroscopebeadanado/MainActivity.kt
+++ app/src/main/java/com/example/gyroscopebeadanado/MainActivity.kt
@@ -1,29 +1,57 @@
 package com.example.gyroscopebeadanado
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.drawable.ShapeDrawable
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import android.view.View
-import com.example.gyroscopebeadanado.MyCanvas
-import java.lang.Float.min
-import java.lang.reflect.Modifier
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(),SensorEventListener {
+    lateinit var mycanvas: MyCanvas
+    lateinit var sensorManager: SensorManager
+    lateinit var sensor: Sensor
+    lateinit var textView: TextView
+    lateinit var acceleroHandler: AcceleroEventHandler
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        mycanvas=findViewById(R.id.draw_field)
+        textView=findViewById(R.id.textView)
 
-        var i= setContentView(MyCanvas(this))
-        
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
 
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+
+
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        if (p0?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            if (p0?.values != null) {
+
+            }
         }
+    }
 
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
 
+    }
 
 
 }
