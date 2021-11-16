4069d2bc81996058eb3b99039e249b8e266e0d3b
Bajkovski <34669317+Bajkovski@users.noreply.github.com>
2021-11-16 22:27:00 +0100

    some color and field limit edit | add orientation handle

--- app/src/main/java/com/example/accelerobeadanado/MainActivity.kt
+++ app/src/main/java/com/example/accelerobeadanado/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.accelerobeadanado
 
-import android.content.res.Configuration
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.view.Display
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(),SensorEventListener {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var mycanvas: MyCanvas
     private lateinit var sensorManager: SensorManager
     private lateinit var sensorAcc: Sensor
     private lateinit var sensorLight: Sensor
     private lateinit var textView: TextView
     private lateinit var textView2: TextView
-    var data = FloatArray(3)
-
-
 
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -31,9 +25,9 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        mycanvas=findViewById(R.id.draw_field)
-        textView=findViewById(R.id.textView)
-        textView2=findViewById(R.id.textView2)
+        mycanvas = findViewById(R.id.draw_field)
+        textView = findViewById(R.id.textView)
+        textView2 = findViewById(R.id.textView2)
         //val height = display!!.height
         //val width = display!!.width
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
@@ -42,8 +36,8 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         sensorAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
-        mycanvas.movex= (display!!.width)/2f
-        mycanvas.movey= (display!!.height)/2f
+        mycanvas.movex = (display!!.width) / 2f
+        mycanvas.movey = (display!!.height) / 2f
 
         //sensorManagerAcc.registerListener(this, sensorAcc, SensorManager.SENSOR_DELAY_FASTEST)
         //sensorManagerLight.registerListener(this, sensorLight, SensorManager.SENSOR_STATUS_ACCURACY_LOW)
@@ -62,20 +56,19 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         sensorManager.unregisterListener(this, sensorAcc)
         sensorManager.unregisterListener(this, sensorLight)
     }
+
     override fun onSensorChanged(p0: SensorEvent?) {
         if (p0?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
             if (p0.values != null) {
                 mycanvas.dataAcc = p0.values
-                textView.text ="x: "+p0.values[0].toString()+"\n"+"y: "+p0.values[1].toString()+"\n"+"z: "+p0.values[2].toString()
-
-
+                textView.text =
+                    "x: " + p0.values[0].toString() + "\n" + "y: " + p0.values[1].toString() + "\n" + "z: " + p0.values[2].toString()
             }
         }
-        if (p0?.sensor?.type== Sensor.TYPE_LIGHT) {
+        if (p0?.sensor?.type == Sensor.TYPE_LIGHT) {
             if (p0.values != null) {
-                mycanvas.dataLight=p0.values[0]
-                textView2.text=p0.values[0].toString()
-
+                mycanvas.dataLight = p0.values[0]
+                textView2.text = p0.values[0].toString()
             }
         }
         mycanvas.invalidate()
--- app/src/main/java/com/example/accelerobeadanado/MyCanvas.kt
+++ app/src/main/java/com/example/accelerobeadanado/MyCanvas.kt
@@ -1,39 +1,25 @@
 package com.example.accelerobeadanado
 
-import android.app.Activity
+
 import android.content.Context
-import android.content.Context.SENSOR_SERVICE
-import android.content.res.Configuration
-import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.os.Build
-import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.Surface
 import android.view.View
-import android.widget.Toast
 import androidx.annotation.RequiresApi
-import kotlin.random.Random
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
 import kotlin.math.absoluteValue
 
 class MyCanvas @JvmOverloads constructor(
     context: Context, attr: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attr, defStyleAttr) {
     var dataAcc = FloatArray(3)
-    var dataLight : Float =0F
+    var dataLight: Float = 0F
     var movex: Float = 0f
     var movey: Float = 0f
     val paint: Paint = Paint()
-
-
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -42,42 +28,47 @@ class MyCanvas @JvmOverloads constructor(
         super.onDraw(canvas)
 
 
-        if(dataLight<200)
+        if (dataLight < 200)
             paint.color = Color.RED
-        if(dataLight>200)
+        if (dataLight > 200)
             paint.color = Color.BLUE
-        if(dataLight<100)
-            paint.color = Color.rgb(dataAcc[0].absoluteValue,dataAcc[1].absoluteValue,dataAcc[2].absoluteValue)
+        if (dataLight < 100)
+            paint.color = Color.rgb(
+                dataAcc[0].absoluteValue,
+                dataAcc[1].absoluteValue,
+                dataAcc[2].absoluteValue
+            )
         paint.style = Paint.Style.FILL
 
-        if(display.rotation==Surface.ROTATION_0)
-        {movex = movex - dataAcc[0] * 0.5f
-        movey = movey + dataAcc[1] * 0.5f}
-        if(display.rotation==Surface.ROTATION_90)
-        {   movex = movex +dataAcc[1] * 0.5f
-         movey = movey + dataAcc[0] * 0.5f}
-        if(display.rotation==Surface.ROTATION_270){
-        movex = movex - dataAcc[1] * 0.5f
-        movey = movey - dataAcc[0] * 0.5f}
+        if (display.rotation == Surface.ROTATION_0) {
+            movex = movex - dataAcc[0] * 0.5f
+            movey = movey + dataAcc[1] * 0.5f
+        }
+        if (display.rotation == Surface.ROTATION_90) {
+            movex = movex + dataAcc[1] * 0.5f
+            movey = movey + dataAcc[0] * 0.5f
+        }
+        if (display.rotation == Surface.ROTATION_270) {
+            movex = movex - dataAcc[1] * 0.5f
+            movey = movey - dataAcc[0] * 0.5f
+        }
 
-        if (movey+50 > height) {
-            movey = height.toFloat()-50f
+        if (movey + 50 > height) {
+            movey = height.toFloat() - 50f
         }
-        if (movex+50 > width) {
-            movex = width.toFloat()-50f
+        if (movex + 50 > width) {
+            movex = width.toFloat() - 50f
         }
-        if (movey-50 < 0) {
+        if (movey - 50 < 0) {
             movey = 50f
         }
-        if (movex-50 < 0) {
+        if (movex - 50 < 0) {
             movex = 50f
         }
         canvas.drawCircle(movex, movey, 50f, paint)
         canvas.save()
 
     }
-
-
 
 
 }
